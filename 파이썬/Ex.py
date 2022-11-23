48967de0dcaf233881a907d5cf1b806b77afbd69
ted <sshted5595@gmail.com>
2022-11-23 17:55:04 +0900

    람다

--- 파이썬/Ex.py
+++ 파이썬/Ex.py
@@ -1,3 +1,10 @@
-number = int(input(">"))
+power = lambda x : x * x
+under_3 = lambda x : x<3
 
-print("{}이다".format(number,number))
+list_input_a = [1,2,3,4,5]
+
+output_a = map(power, list_input_a)
+print(list(output_a))
+
+output_b = filter(under_3, list_input_a)
+print(list(output_b))
